80cabd9dcee61d78287084edb9c9038255de60b0
farhinsheikh <farhinsheikh2015@gmail.com>
2021-04-21 00:01:21 +0530

    [Farhin]ADDED:first name

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserValidation.FirstName();
+        }
+    }
+}
---
+++ UserValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegistrationProblem
+{
+    class UserValidation
+    {
+        public static void FirstName()
+        {
+            string regex = "^[A-Za-z]{3,}$";
+            Console.WriteLine("Enter first name");
+            string fName = Console.ReadLine();
+            Regex rg = new Regex(regex);
+            bool result = rg.IsMatch(fName);
+            if (result == true)
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
